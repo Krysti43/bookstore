a6d2629223aa06b73ad31a7b826653060391bf2a
krystian <krystianskiba8@wp.pl>
2026-06-17 14:28:37 +0200

    Dodanie service book i zmiany w Bookcontroller

---
+++ src/main/java/com/example/bookstore/service/BookService.java
@@ -0,0 +1,21 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.entity.Book;
+import com.example.bookstore.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
