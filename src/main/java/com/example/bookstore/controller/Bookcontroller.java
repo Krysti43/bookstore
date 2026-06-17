703961a9c5a0f4cc4787e5b9b6e7863404086e8f
krystian <krystianskiba8@wp.pl>
2026-06-17 14:06:44 +0200

    Dodanie controllera książek

---
+++ src/main/java/com/example/bookstore/controller/Bookcontroller.java
@@ -0,0 +1,13 @@
+ppackage com.example.bookstore.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Bookstore API";
+    }
+}
