9bcd8a1304e415704e9fe26540229808c045734b
krystian <krystianskiba8@wp.pl>
2026-06-17 14:13:16 +0200

    Poprawa błędów z controllerami

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -1,10 +1,10 @@
-ppackage com.example.bookstore.controller;
+package com.example.bookstore.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class BookController {
 
     @GetMapping("/hello")
     public String hello() {
