994a482757308678f2932ed4ecb99a10c305e8a0
krystian <krystianskiba8@wp.pl>
2026-06-17 17:11:57 +0200

    Zabezpieczenie endpointów na przyszłe dodanie user/admin

--- src/main/java/com/example/bookstore/config/SecurityConfig.java
+++ src/main/java/com/example/bookstore/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.bookstore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,8 +14,16 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .requestMatchers(
+                                "/auth/register",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+                        .requestMatchers("/books/**").permitAll()
+                        .requestMatchers("/reservations/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
